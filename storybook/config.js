b373bb284c6f0595c9135144e6b75f2044ff5c5a
sadcitizen <sadcitizen@yandex.ru>
2017-01-10 14:13:39 +0500

    add storybook base config + addons

---
+++ storybook/config.js
@@ -0,0 +1,9 @@
+import { configure } from '@kadira/storybook';
+
+/* eslint-disable global-require */
+function loadStories() {
+
+}
+/* eslint-enable global-require */
+
+configure(loadStories, module);
