c469089c92150838e62027b123a7f235144f2ea4
sadcitizen <sadcitizen@yandex.ru>
2017-01-10 14:29:41 +0500

    loading stories dynamically

--- storybook/config.js
+++ storybook/config.js
@@ -1,8 +1,10 @@
 import { configure } from '@kadira/storybook';
 
+const req = require.context('../src', true, /stories.js$/);
+
 /* eslint-disable global-require */
 function loadStories() {
-
+    req.keys().forEach(filename => req(filename));
 }
 /* eslint-enable global-require */
 
